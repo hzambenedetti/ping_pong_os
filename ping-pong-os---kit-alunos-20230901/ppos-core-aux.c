e5bf0aed660f00d1063f7faca8a607a9237e2c80
HenriqueZ18 <Hique.rz03@gmail.com>
2024-05-12 19:26:15 -0300

    Added line to after_task_switch to increment the tasks activations.

--- ping-pong-os---kit-alunos-20230901/ppos-core-aux.c
+++ ping-pong-os---kit-alunos-20230901/ppos-core-aux.c
@@ -231,6 +231,7 @@ void before_task_switch ( task_t *task ) {
 
 void after_task_switch ( task_t *task ) {
     // put your customization here
+    task->activations++;
 #ifdef DEBUG
     printf("\ntask_switch - AFTER - [%d -> %d]", taskExec->id, task->id);
 #endif
